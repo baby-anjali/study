407bbd7efd1b549d27c168ad7bfd4807877a16ed
baby-anjali <183719049+baby-anjali@users.noreply.github.com>
2025-01-04 21:45:50 +0530

    tested for product function

--- arrays_test.js
+++ arrays_test.js
@@ -1,4 +1,4 @@
-import { assert, assertFalse, assertEquals } from "jsr:@std/assert";
+import { assertEquals } from "jsr:@std/assert";
 import {
   product,
   filterTruthy,
@@ -6,3 +6,27 @@ import {
   sortByLength,
   squareRoots,
 } from "./arrays.js";
+
+Deno.test("Product of elements with empty array", () => {
+  assertEquals(product([]), 1);
+});
+
+Deno.test("Product of elements that are all positive", () => {
+  assertEquals(product([1, 2, 3]), 6);
+  assertEquals(product([1, 2]), 2);
+  assertEquals(product([1]), 1);
+});
+
+Deno.test("Product of elements where some are negative", () => {
+  assertEquals(product([1, 2, -3]), -6);
+  assertEquals(product([-1, -2]), 2);
+  assertEquals(product([9, -7, -8]), 504);
+  assertEquals(product([-1]), -1);
+});
+
+Deno.test("Product of elements where there is zero", () => {
+  assertEquals(product([1, 0, -3]), 0);
+  assertEquals(product([-1, 0]), 0);
+  assertEquals(product([0, -7, -8]), 0);
+  assertEquals(product([0]), 0);
+});
