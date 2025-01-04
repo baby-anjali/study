b8499f9bf6234b9680b76f9968c2ea16a4e9b448
baby-anjali <183719049+baby-anjali@users.noreply.github.com>
2025-01-04 17:41:42 +0530

    created regexes for number extract, startEnd, duplicates

--- regex.js
+++ regex.js
@@ -4,3 +4,13 @@ const emailValidate = /\w+@g?mail.com/;
 // 2. Create a regex pattern to match a U.S. phone number format:
 // `(123) 456-7890`.
 const usPhoneValidate = /\(\d{3}\) \d{3}-\d{4}/;
+
+// 3. Extract all the numbers from the string `"abc123def456ghi789"`.
+const allNumbers = /\d+/g;
+
+// 4. Validate if a string starts with `"Hello"` and ends with `"World!"`.
+const startEnd = /^Hello.*World!$/;
+
+// 5. Write a regex to find duplicate words in the string: 
+// `"This is is a test test string."`.
+const duplicates = /\b(\w+)\b.*\b\1\b/;
