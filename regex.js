c2b38f6d71f0fe263eb49042a66241dc458fa9b4
baby-anjali <183719049+baby-anjali@users.noreply.github.com>
2025-01-04 17:35:19 +0530

    created regexes for mail and phone validations

---
+++ regex.js
@@ -0,0 +1,6 @@
+// 1. Write a regular expression to validate an email address.
+const emailValidate = /\w+@g?mail.com/;
+
+// 2. Create a regex pattern to match a U.S. phone number format:
+// `(123) 456-7890`.
+const usPhoneValidate = /\(\d{3}\) \d{3}-\d{4}/;
