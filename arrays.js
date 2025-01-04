b605cb24cce5308ff945564b728599c6662469d0
baby-anjali <183719049+baby-anjali@users.noreply.github.com>
2025-01-04 21:35:28 +0530

    export array functions

--- arrays.js
+++ arrays.js
@@ -1,12 +1,13 @@
 //6. Use `reduce` to calculate the product of all elements in
 // `[1, 2, 3, 4, 5]`
 
-const product = (array) => array.reduce((init, element) => init * element, 1);
+export const product = (array) =>
+  array.reduce((init, element) => init * element, 1);
 
 // 7. Write a function to remove all falsy values from an array:
 // `[0, 1, false, 2, '', 3, null, 4, undefined]`.
 
-const filterTruthy = (array) => array.filter((element) => element);
+export const filterTruthy = (array) => array.filter((element) => element);
 
 // 8. Use `filter` to extract all prime numbers from
 // `[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]`.
@@ -25,12 +26,14 @@ const isPrime = function (number) {
   return true;
 };
 
-const filterPrimes = (array) => array.filter((element) => isPrime(element));
+export const filterPrimes = (array) =>
+  array.filter((element) => isPrime(element));
 
 // 9. Sort an array of strings by their length.
 
-const sortByLength = (array) => array.sort((a, b) => a.length - b.length);
+export const sortByLength = (array) =>
+  array.sort((a, b) => a.length - b.length);
 
 // 10. Use `map` to create an array of the square roots of `[1, 4, 9, 16, 25]`.
 
-const squareRoots = (array) => array.map((number) => Math.sqrt(number));
+export const squareRoots = (array) => array.map((number) => Math.sqrt(number));
