a7a1ea158a24ed757b55eb13a8c60d812cc8e5aa
baby-anjali <183719049+baby-anjali@users.noreply.github.com>
2025-01-04 22:14:39 +0530

    tested for filterPrimes function

--- arrays_test.js
+++ arrays_test.js
@@ -56,6 +56,30 @@ Deno.test("Filter truthy for array with no truthy values", () => {
   assertEquals(filterTruthy([false]), []);
   assertEquals(filterTruthy([null]), []);
   assertEquals(filterTruthy([undefined]), []);
-  assertEquals(filterTruthy([NaN]), []),
-    assertEquals(filterTruthy([0, null, undefined, false, 0, "", NaN]), []);
+  assertEquals(filterTruthy([NaN]), []);
+  assertEquals(filterTruthy([0, null, undefined, false, 0, "", NaN]), []);
+});
+
+Deno.test("Filter primes of empty array", () => {
+  assertEquals(filterPrimes([]), []);
+});
+
+Deno.test("Filter primes for array with all primes", () => {
+  assertEquals(filterPrimes([2, 3, 5, 7]), [2, 3, 5, 7]);
+  assertEquals(filterPrimes([2, 5, 7]), [2, 5, 7]);
+  assertEquals(filterPrimes([5, 7]), [5, 7]);
+  assertEquals(filterPrimes([7]), [7]);
+});
+
+Deno.test("Filter primes for array with some primes", () => {
+  assertEquals(filterPrimes([1, 9, 2, 3, 5, 7]), [2, 3, 5, 7]);
+  assertEquals(filterPrimes([2, 5, 0, 8, 7]), [2, 5, 7]);
+  assertEquals(filterPrimes([5, 7, 111]), [5, 7]);
+  assertEquals(filterPrimes([7, 81]), [7]);
+});
+
+Deno.test("Filter primes for array with no primes", () => {
+  assertEquals(filterPrimes([1, 9, 111]), []);
+  assertEquals(filterPrimes([1, 9]), []);
+  assertEquals(filterPrimes([1]), []);
 });
